3a4a1ade5c41151573dfddc68744a983dbdfb019
JP Whitaker <jpw@hawaii.edu>
2023-03-02 18:52:51 -1000

    working scene with oceane text

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,34 +1,46 @@
 import * as THREE from 'three'
-import img from '../public/ground.png'
-import { Suspense } from 'react'
-import { useLoader } from '@react-three/fiber'
-import { OrbitControls, Stage, Sparkles } from '@react-three/drei'
+import { Suspense, useState } from 'react'
+import { useLoader, useFrame } from '@react-three/fiber'
+import { OrbitControls, Stage, Sparkles, useVideoTexture, Text3D, Center } from '@react-three/drei'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import UnderwaterObjects from './UnderwaterObjects'
+import { PlainAnimator } from "three-plain-animator/lib/plain-animator"
+import Seaweed01 from './meshes/Seaweed01'
+import Seaweed02 from './meshes/Seaweed02'
+import Seaweed03 from './meshes/Seaweed03'
+import Crab from './meshes/Crab'
+import Rock01 from './meshes/Rock01'
 
-function Image() {
-  const texture = useLoader(THREE.TextureLoader, img)
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.set( 4, 4 );
+
+
+
+
+
+function Sprite({ textureSrc, IconPosition, IconSize }) {
+  const spriteTexture = useLoader(THREE.TextureLoader, textureSrc)
+  const [animator] = useState(() => new PlainAnimator(spriteTexture, 4, 4, 10, 10))
+  useFrame(() => animator.animate())
+  spriteTexture.needsUpdate = true;
   return (
-    <mesh position-y={-2} rotation-x={ - Math.PI * 0.5} scale={10}>
-      <planeBufferGeometry attach="geometry"  />
-      <meshStandardMaterial attach="material" map={texture} transparent={true} roughness={0.10}/>
-    </mesh>
+      <spotLight position={[0, 10, 0]} intensity={1} color="#fff" map={spriteTexture}  />
   )
 }
 
 export default function Experience() {
 
   const model = useLoader(GLTFLoader, './uploads_files_3661723_Tile_pack_environment_4.glb')
-  
+  const colorMap = useLoader(TextureLoader, './3s0F.gif')
+  const videoTexture = useVideoTexture("./water-grayscale-darker.mp4")
+  videoTexture.needsUpdate = true;
+
   return (
     <>
-      <fog attach="fog" color={`#0fa2ab`} near={-30} far={80} />
+      <fog attach="fog" color={`#0fa2ab`} near={-80} far={80} />
       <Sparkles
+        position={[0,20,0]}
         /** Number of particles (default: 100) */
-        count = {300}
+        count = {4000}
         /** Speed of particles (default: 1) */
         speed = {0.1}
         /** Opacity of particles (default: 1) */
@@ -38,31 +50,39 @@ export default function Experience() {
         /** Size of particles (default: randomized between 0 and 1) */
         size = {1}
         /** The space the particles occupy (default: 1) */
-        scale = {10}
+        scale = {40}
         /** Movement factor (default: 1) */
       />
-      <directionalLight color="#FFFFFF" position={[20, 40, 0]} castShadow />
-      <ambientLight intensity={0.5} />
       
-        <OrbitControls/>
-        {/* <pointLight position={[0, 500, 0]} intensity={.5} color="#fff" /> */}
+      <spotLight position={[0, 20, 0]} intensity={1} color="#fff" map={videoTexture} penumbra={0.5} castShadow />
+      <ambientLight intensity={0.4} />
+      
+        <OrbitControls maxPolarAngle = {Math.PI/2}/>
 
-        {/* <Suspense fallback={null}>
-          <Image />
-        </Suspense> */}
-        <mesh position={[0, 1, 0]} castShadow>
+
+        {/* <mesh position={[0, 1, 0]} castShadow>
           <sphereGeometry 
           />
           <meshStandardMaterial color="red" />
-        </mesh>
+        </mesh> */}
         
-        <mesh position-y={0} rotation-x={ - Math.PI * 0.5} scale={200} receiveShadow>
+        <mesh position-y={0} rotation-x={ - Math.PI * 0.5} scale={250} receiveShadow>
           <planeGeometry/>
           <meshStandardMaterial color="yellow"/>
         </mesh>
 
         {/* <primitive object={model.scene}/> */}
-        <UnderwaterObjects position={[-15,-1,0]}/>
+        <Seaweed01 position={[-3,0,0]}/>
+        <Seaweed02 position={[-9.5,0,-7]}/>
+        <Seaweed03 position={[-8,0,-6]}/>
+        <Crab position={[1,0,-1]}/>
+        <Rock01 position={[0,0,-2]}/>
+      <Center disableY>
+        <Text3D font="./Kalam_Bold.json" position={[0,2,0]}>
+          Oćeane
+          <meshLambertMaterial/>
+        </Text3D>
+       </Center>
       
     </>
   )
