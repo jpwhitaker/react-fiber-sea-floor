71f700611066221c39d08d827b352a233489711a
JP Whitaker <jpw@hawaii.edu>
2023-03-05 10:34:39 -1000

    basic mobile

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,23 @@ import { StrictMode } from 'react'
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import { Loader } from '@react-three/drei'
 import Experience from './Experience'
+import ExperienceMobile from './ExperienceMobile'
+import { useMediaQuery } from '@react-hook/media-query';
+
+function Root() {
+    const isDesktop = useMediaQuery('(min-width: 768px)');
+  
+    if (!isDesktop) {
+      return <ExperienceMobile />; // only render ExperienceMobile on mobile screens
+    }
+  
+    return (
+      <>
+        <Experience />
+      </>
+    );
+  }
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -11,7 +26,7 @@ root.render(
     <StrictMode>
         <Canvas shadows>
             <color attach="background" args={["#0fa2ab"]}/>
-            <Experience/>
+            <Root/>
         </Canvas>
     </StrictMode>
 )
