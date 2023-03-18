ea72d1591051fd10a7a83f7f7638baeb23b1b9e9
JP Whitaker <jpw@hawaii.edu>
2023-03-18 13:50:41 -1000

    anchoring and starting gizmo at box origin

--- src/GroundWire.jsx
+++ src/GroundWire.jsx
@@ -1,33 +1,45 @@
-import { useMediaQuery } from "@react-hook/media-query";
 import { useRef } from "react";
-import { PivotControls, QuadraticBezierLine} from "@react-three/drei";
+import { PivotControls, QuadraticBezierLine, Box} from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { Vector3 } from "three";
-
+import { Vector3, Object3D } from "three";
+import { Matrix4 } from "three";
 
 export default function GroundWire() {
-  const isDesktop = useMediaQuery('(min-width: 768px)');
-  const pivotControlsRef = useRef();
+  const pivotControlsRef1 = useRef();
+  const pivotControlsRef2 = useRef();
+  const object3DRef1 = useRef();
+  const object3DRef2 = useRef();
+  const wireStart = useRef(new Vector3());
+  const wireEnd = useRef(new Vector3());
+  const initialPosition1 = [1, 1, 1];
+  const initialPosition2 = [0, 0, 0];
+  const gizmoMatrix1 = new Matrix4();
+  gizmoMatrix1.setPosition(1, 1, 1);
 
- 
-  useFrame(() => {
-    if (pivotControlsRef.current) {
-      //do something
-    }
-  });
+
+  // useFrame(() => {
+  //   if (pivotControlsRef1.current && pivotControlsRef2.current) {
+  //     wireStart.current.copy(object3DRef1.current.position);
+  //     wireEnd.current.copy(object3DRef2.current.position);
+  //   }
+  // });
 
   return (
     <>
-      <PivotControls ref={pivotControlsRef} mode="translate">
-        <Wire end={[0,0,0]} start={[1,1,1]}/>
-        <object3D/>
+      <PivotControls ref={pivotControlsRef1} mode="translate" matrix={gizmoMatrix1} anchor={[0,0,0]}>
+        <Box ref={object3DRef1} position={initialPosition1} />
       </PivotControls>
+
+      
+      <PivotControls ref={pivotControlsRef2} mode="translate" position={initialPosition2}>
+        <Box ref={object3DRef2} position={initialPosition2} />
+      </PivotControls>
+      <Wire start={wireStart.current} end={wireEnd.current} />
     </>
-  )
+  );
 }
 
-
-const Wire = function({start, end}){
+const Wire = function ({ start, end }) {
   return (
     <QuadraticBezierLine
       start={start}
@@ -35,7 +47,6 @@ const Wire = function({start, end}){
       lineWidth={4}
       fog={true}
       color={"#474747"}
-    >
-    </QuadraticBezierLine> 
-  )
-}
+    ></QuadraticBezierLine>
+  );
+};
