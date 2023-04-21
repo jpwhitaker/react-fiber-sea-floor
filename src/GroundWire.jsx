67241ec0440232c7528c758bfb0228e55f2b0490
JP Whitaker <jpw@hawaii.edu>
2023-04-20 23:43:35 -1000

    wip on wire work, stopping to convert to collectivsea

--- src/GroundWire.jsx
+++ src/GroundWire.jsx
@@ -1,47 +1,62 @@
-import { useRef } from "react";
+import { useRef, forwardRef } from "react";
 import { PivotControls, QuadraticBezierLine, Box} from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { Vector3, Object3D } from "three";
 import { Matrix4 } from "three";
 
 export default function GroundWire() {
-  const pivotControlsRef1 = useRef();
-  const pivotControlsRef2 = useRef();
-  const object3DRef1 = useRef();
-  const object3DRef2 = useRef();
-  const wireStart = useRef(new Vector3());
-  const wireEnd = useRef(new Vector3());
+
+  const boxRef1 = useRef();
+  const boxRef2 = useRef();
+  const gizmoRef1 = useRef();
+  const gizmoRef2 = useRef();
+  const wireRef = useRef();
+  
+
   const initialPosition1 = [1, 1, 1];
   const initialPosition2 = [0, 0, 0];
+  
   const gizmoMatrix1 = new Matrix4();
   gizmoMatrix1.setPosition(1, 1, 1);
 
 
-  // useFrame(() => {
-  //   if (pivotControlsRef1.current && pivotControlsRef2.current) {
-  //     wireStart.current.copy(object3DRef1.current.position);
-  //     wireEnd.current.copy(object3DRef2.current.position);
-  //   }
-  // });
+  useFrame(() => {
+    
+    if (boxRef1.current && boxRef2.current && wireRef.current) {
+      const position1 = boxRef1.current.position
+      const position2 = boxRef2.current.position
+      wireRef.current.setPoints(position1, position2)
+      // console.log(`1: ${boxRef1.current.position.toArray()} 2. ${boxRef2.current.position.toArray()}`)
+      // console.log(`1: ${gizmoRef1.current.position.toArray()} 2. ${gizmoRef2.current.position.toArray()}`)
+      // debugger
+      
+    }
+  });
 
   return (
     <>
-      <PivotControls ref={pivotControlsRef1} mode="translate" matrix={gizmoMatrix1} anchor={[0,0,0]}>
-        <Box ref={object3DRef1} position={initialPosition1} />
+      <PivotControls ref={gizmoRef1} onDrag={(e) => {
+        const pos = new Vector3().setFromMatrixPosition(e); 
+        console.log(pos);
+        //logging the position but wire isnt updating.  Look to SlackWire implementation in SubAndMic
+        wireRef.current.setPoints([0,0,0], pos)
+        }} autoTransform={true} mode="translate" matrix={gizmoMatrix1} anchor={[0,0,0]}>
+        <Box ref={boxRef1} position={initialPosition1} />
       </PivotControls>
 
       
-      <PivotControls ref={pivotControlsRef2} mode="translate" position={initialPosition2}>
-        <Box ref={object3DRef2} position={initialPosition2} />
+      <PivotControls ref={gizmoRef2} autoTransform={true} mode="translate" anchor={[0,0,0]}>
+        <Box ref={boxRef2} position={initialPosition2} />
       </PivotControls>
-      <Wire start={wireStart.current} end={wireEnd.current} />
+      <Wire ref={wireRef} start={[0,0,0]} end={[2,2,2]} />
     </>
   );
 }
 
-const Wire = function ({ start, end }) {
+const Wire = forwardRef(({ start, end }, ref) =>{
   return (
     <QuadraticBezierLine
+      ref={ref}
       start={start}
       end={end}
       lineWidth={4}
@@ -49,4 +64,4 @@ const Wire = function ({ start, end }) {
       color={"#474747"}
     ></QuadraticBezierLine>
   );
-};
+});
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -32,7 +32,7 @@ export function NavBar() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                    <span className='text-white text-md font-medium'>Océane Boulais</span>
+                    <span className='text-white text-md font-medium'>CollectivSea</span>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
