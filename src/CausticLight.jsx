3fbfaed7e76edf160c646116b8c5100b5e854863
JP Whitaker <jpw@hawaii.edu>
2023-04-21 11:32:25 -1000

    wrapped video in suspense

--- src/CausticLight.jsx
+++ src/CausticLight.jsx
@@ -2,21 +2,17 @@ import { Suspense, useMemo, useRef } from 'react'
 import { SpotLight } from 'three'
 import {useVideoTexture, useTexture } from '@react-three/drei'
 
+
 export default function CausticLight() {
   let videoTexture = null;
-  {/**There has to be a better way to get the video to load consistently */}
-  try {
-    videoTexture = useVideoTexture("./water_1_lite_1.mp4");
-    
-  } catch (error) {
-    console.error('Failed to load video texture', error);
-  }
-    
+  
+  videoTexture = useVideoTexture("./water_1_lite_1.mp4");
 
     const causticLight = useMemo(() => new SpotLight('#fff'), []);
     
     if (videoTexture != null){
       return <group>
+        <Suspense fallback={<meshBasicMaterial wireframe />}>
           <primitive
             object={causticLight}
             position={[0, 20, 2]}
@@ -25,6 +21,7 @@ export default function CausticLight() {
             castShadow={false}
             penumbra={0.5} />
           <primitive object={causticLight.target} position={[0, 0, 0]} />
+        </Suspense>
       </group>
     } else {
       return null
